94041d816beb52bb9e054f3edc457e261a38f5ca
FelizYK <1694493710@qq.com>
2024-09-07 23:03:48 +0800

    add mysql logger

--- internal/sql/sql.go
+++ internal/sql/sql.go
@@ -2,30 +2,30 @@ package sql
 
 import (
 	"fmt"
-	// "log"
-	// "time"
+	"log"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	// "gorm.io/gorm/logger"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func InitMySQL() {
-	// mylogger := logger.New(
-	// 	log.New(log.Writer(), "\r\n", log.LstdFlags),
-	// 	logger.Config{
-	// 		SlowThreshold: time.Second,
-	// 		LogLevel:      logger.Info,
-	// 		Colorful:      true,
-	// 	},
-	// )
+	mylogger := logger.New(
+		log.New(log.Writer(), "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
 
 	var err error
 	// DB, err = gorm.Open(mysql.Open(viper.GetString("mysql.dsn")), &gorm.Config{})
 	dsn := "root:password@tcp(localhost:3306)/gim_db?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{ /* Logger: mylogger */ })
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: mylogger})
 	if err != nil {
 		panic(err)
 	}
